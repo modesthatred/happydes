069d2f1c2352475313060ef828d58b01096b55b3
modesthatred <modesthatred@users.noreply.github.com>
2018-08-15 13:31:59 +0300

    fix colour,position and diplicate classes for sliders arrow

--- js/all.js
+++ js/all.js
@@ -24,6 +24,11 @@ jQuery(document).ready(function ($) {
         });
     });
 
+    lightbox.option({
+      'resizeDuration': 200,
+      'wrapAround': true
+    })
+
     $('.x-front-page-slider').each(function(i) {
         var $this = $(this);
 
@@ -71,6 +76,14 @@ jQuery(document).ready(function ($) {
         }],
     });
 
+//лайтбоксы 
+    lightbox.option({
+      'alwaysShowNavOnTouchDevices': false,
+      //вписать или растянуть
+      // 'fitImagesInViewport':  false,
+    })
+
+
     $('.x-resume-slider').slick({
         infinite: true,
         speed: 300,
@@ -79,8 +92,8 @@ jQuery(document).ready(function ($) {
         useCSS: true,
         dots: true,
         arrow: false,
-        prevArrow: '<div class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" width="16.813" height="31.91" viewBox="0 0 16.813 31.91"><path d="M.967 31.907c-.253 0-.506-.097-.7-.292-.386-.39-.386-1.02 0-1.41l14.148-14.252L.268 1.7C-.12 1.313-.12.68.268.29c.386-.388 1.013-.388 1.4 0L16.514 15.25c.386.39.386 1.02 0 1.41L1.667 31.615c-.193.195-.446.292-.7.292z" class="cls-1"/></svg></div>',
-        nextArrow: '<div class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" width="16.813" height="31.91" viewBox="0 0 16.813 31.91"><path d="M.967 31.907c-.253 0-.506-.097-.7-.292-.386-.39-.386-1.02 0-1.41l14.148-14.252L.268 1.7C-.12 1.313-.12.68.268.29c.386-.388 1.013-.388 1.4 0L16.514 15.25c.386.39.386 1.02 0 1.41L1.667 31.615c-.193.195-.446.292-.7.292z" class="cls-1"/></svg></div>',
+        prevArrow: '<div class="slick-prev-resume"><svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" width="16.813" height="31.91" viewBox="0 0 16.813 31.91"><path d="M.967 31.907c-.253 0-.506-.097-.7-.292-.386-.39-.386-1.02 0-1.41l14.148-14.252L.268 1.7C-.12 1.313-.12.68.268.29c.386-.388 1.013-.388 1.4 0L16.514 15.25c.386.39.386 1.02 0 1.41L1.667 31.615c-.193.195-.446.292-.7.292z" class="cls-1"/></svg></div>',
+        nextArrow: '<div class="slick-next-resume"><svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" width="16.813" height="31.91" viewBox="0 0 16.813 31.91"><path d="M.967 31.907c-.253 0-.506-.097-.7-.292-.386-.39-.386-1.02 0-1.41l14.148-14.252L.268 1.7C-.12 1.313-.12.68.268.29c.386-.388 1.013-.388 1.4 0L16.514 15.25c.386.39.386 1.02 0 1.41L1.667 31.615c-.193.195-.446.292-.7.292z" class="cls-1"/></svg></div>',
         slidesToShow: 4,
         slidesToScroll: 4,
         swipeToSlide: 1,
